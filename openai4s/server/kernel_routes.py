05201f9b499a0742f828ad815623744e2aba04d9
Yu Wang <70514121+wangyu-sd@users.noreply.github.com>
2026-08-12 19:23:59 +0800

    style(server): use package-relative route imports

--- openai4s/server/kernel_routes.py
+++ openai4s/server/kernel_routes.py
@@ -37,8 +37,8 @@ from __future__ import annotations
 import re
 from typing import Any
 
-from openai4s.server.contract import RouteSpec  # isort: split
-from openai4s.server.errors import GatewayError
+from .contract import RouteSpec
+from .errors import GatewayError
 
 
 _EXECUTION = RouteSpec("kernel.execution", "GET", r"/frames/([^/]+)/execution")
